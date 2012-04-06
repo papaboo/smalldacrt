338861b3e32ee707a10d2d0c60751160ab96ce55
Asger Dam Hoedt <asger@unity-studios.com>
2012-04-06 21:35:16 +0200

    Moved some stuff from Utils to Math

--- Utils.h
+++ Utils.h
@@ -11,15 +11,6 @@
 #include "Cone.h"
 #include "Vector.h"
 
-template <class T>
-inline T Clamp01(T v) { 
-    return v < T(0) ? T(0) : v > T(1) ? T(1) : v;
-}
-
-inline float Rand01() {
-    return (float)rand() / (float)RAND_MAX;
-}
-
 inline int ToByte(float v) {
     return int(pow(Clamp01(v),1/2.2)*255+.5);
 }
